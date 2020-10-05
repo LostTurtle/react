cda2080e7a0af4fdd1f8f3b910604feee9c7efd0
LostTurtle <khaloodm25@gmail.com>
2020-10-06 01:59:23 +0200

    react navbar

--- initial-start/src/App.js
+++ initial-start/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+      </Navbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
