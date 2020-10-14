3ec4008f9feaedd90601e21376da91e6881d31ca
Khaled.Mohamed <61638424+LostTurtle@users.noreply.github.com>
2020-10-15 00:43:16 +0200

    Update App.js

--- initial-start/src/App.js
+++ initial-start/src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
